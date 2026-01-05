b287564807608efc91e126ffa34d207b2c15c345
Jing1224 <JingPro1224@gmail.com>
2026-01-05 17:55:33 -0500

    Add basic layout with Header, Sidebar, and Canvas

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Canvas() {
+  return (
+    <div className='canvas-area'>
+      <h2>Canvas</h2>
+      <p>No image loaded</p>
+    </div>
+  );
+}
+
+export default Canvas;
+
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Sidebar()
+{
+  return (
+    <div className="sidebar">
+      <h2>Images</h2>
+      <p>No images yet</p>
+    </div>
+  );
+}
+
+export default Sidebar;
